a4a1b5d8bed398c7d71578f482c3f5ae3132a608
Dawidd95 <32812931+Dawidd95@users.noreply.github.com>
2018-07-03 17:57:28 +0200

    Add files via upload

---
+++ scripts/smooth-scrolling.js
@@ -0,0 +1,13 @@
+$(function() { // DOMContentLoaded
+  $(".page-head__button").on("click", function() {  //event listener
+    $("html, body").animate({
+      scrollTop: $(".container__offers").offset().top //dopisujac -300 bedzie do klasy odjac 300px
+    }, 2000);
+  });
+
+  $(".text-container__button").on("click", function() {
+    $("html,body").animate({
+      scrollTop: $(".contact-container").offset().top
+    }, 2000);
+  });
+});
